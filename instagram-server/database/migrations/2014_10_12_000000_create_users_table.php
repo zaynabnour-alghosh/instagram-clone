38f031385bb825782a890c7ad2e840797d2d4c09
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-09 10:46:30 +0300

    Server <> Created Migrations

--- instagram-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ instagram-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,20 +6,66 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->text('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->text('image_url')->nullable();
+            $table->text('bio')->nullable();
+            $table->integer('nb_followers');
+            $table->integer('nb_followings');
+            $table->integer('nb_posts');
+            $table->timestamps();
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->text('image_url')->nullable();           
+            $table->integer('nb_likes');
+            $table->timestamps();
+        });
+
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('post_id');           
+            $table->timestamps();
+        });
+
+        Schema::create('unlikes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('post_id');           
+            $table->timestamps();
+        });
+
+        Schema::create('follows', function (Blueprint $table) {
+            $table->id();
+            $table->integer('follower_id');
+            $table->integer('following_id');           
+            $table->timestamps();
+        });
+
+        Schema::create('unfollows', function (Blueprint $table) {
+            $table->id();
+            $table->integer('unfollower_id');
+            $table->integer('unfollowing_id');           
+            $table->timestamps();
+        });
+
     }
 
     /**
