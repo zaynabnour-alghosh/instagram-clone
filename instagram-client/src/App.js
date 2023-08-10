522af78a4c6ae8b5fae4e0a0a13bb3c119c65ece
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-10 13:32:13 +0300

    Client <> Implemented Design of SideBar Component

--- instagram-client/src/App.js
+++ instagram-client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 // import Login from './components/Login';
-import Signup from './components/Signup';
+// import Signup from './components/Signup';
+import SideBar from './components/SideBar';
 function App() {
   return (
     <div className="App">  
     {/* <Login />   */}
-    <Signup /> 
+    {/* <Signup />  */}
+    <SideBar />
     </div>
   );
 }
---
+++ instagram-client/src/components/SideBar/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./sidebar.css";
+import instaName from '../../images/name.png';
+import homeIcon from '../../images/home.png';
+import searchIcon from '../../images/search.png';
+import postIcon from '../../images/create.png';
+import profileIcon from '../../images/pro.png';
+const SideBar=()=>{
+    return (
+        <div className="sidebar">
+           <div className="container">
+            <div className="header">
+            <img src={instaName} alt="" />
+            </div>
+            <div className="nav">
+                <div className="icon">
+                    <img src={homeIcon} alt="" />Home
+                </div>
+                <div className="icon">
+                    <img src={searchIcon} alt="" />Search
+                </div>
+                <div className="icon">
+                    <img src={postIcon} ></img>Create
+                </div>
+                <div className="icon">
+                    <img src={profileIcon} alt="" /> Profile
+                </div>
+            </div>
+           </div>
+        </div>
+    );
+}
+export default SideBar;
