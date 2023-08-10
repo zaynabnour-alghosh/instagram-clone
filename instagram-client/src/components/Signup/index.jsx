667cd6468317703b45e25ff32687a9b51801541c
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-10 20:46:44 +0300

    Client <> Implemented Login and Signup --Axios

--- instagram-client/src/components/Signup/index.jsx
+++ instagram-client/src/components/Signup/index.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useState } from "react";
 import "./signup.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import instaName from '../../images/name.png';
 const Signup=()=>{
+    const navigate = useNavigate();
+    const [data,setData]=useState({
+        email:"Email",
+        name:"Full Name",
+        username:"Username",
+        password:"Password"
+    })
+    const handleDataChange=(e)=>{
+        setData({...data,[e.target.name]:e.target.value});
+        }
+
+    const handleSignup=async()=>{
+        try{
+            const response=await axios.post("http://127.0.0.1:8000/api/guest/register",data)
+            if(response){
+                const result=response.data;
+                console.log(result)
+                navigate('/');
+            }
+            
+            
+        }catch(e){console.log(e)}
+    }
+
+
+
+
+
     return (
         <div className="signup">
             <div className="container">
@@ -11,19 +42,19 @@ const Signup=()=>{
                 </div>
                 <div className="content">
                     <div className="row">
-                        <input type="email" placeholder="Email" />
+                        <input type="email" placeholder="Email" name="email"  onChange={handleDataChange}/>
                     </div>
                     <div className="row">
-                        <input type="text" placeholder="Full Name" />
+                        <input type="text" placeholder="Full Name" name="name" onChange={handleDataChange}/>
                     </div>
                     <div className="row">
-                        <input type="text" placeholder="Username" />
+                        <input type="text" placeholder="Username" name="username" onChange={handleDataChange}/>
                     </div>
                     <div className="row">
-                        <input type="password" placeholder="Password" />
+                        <input type="password" placeholder="Password" name="password" onChange={handleDataChange}/>
                     </div>
                     <div className="row">
-                        <button>Sign up</button>
+                        <button onClick={handleSignup}>Sign up</button>
                     </div>
                 </div>
             </div>
