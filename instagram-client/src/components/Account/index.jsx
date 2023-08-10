e58bcfa187c51001d1e55ea722d6b7b5cfd48cf2
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-10 23:32:30 +0300

    Client <> Editing Account Component

--- instagram-client/src/components/Account/index.jsx
+++ instagram-client/src/components/Account/index.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import "./account.css";
-// import Profile from "../Profile";
-
+import Profile from "../Profile";
+import SideBar from "../SideBar";
 
 const Account=()=>{
     return(
         <div className="acc">
+            <SideBar />
             <div className="acc-container">
+                
                 <div className="pro-content">
-                    {/* <Profile /> */}
+                    <Profile />
                 </div>
-               <div className="pContent">
-                <div className="pleft"></div>
-                <div className="p">POSTS</div>
-                <div className="pright"></div>
-              </div>
-              <div className="posts">
-              <img src="https://tse4.mm.bing.net/th?id=OIP.Z_FV1UU_1YaB7KcXHDa6_gHaKO&pid=Api&P=0&h=180" alt="post" />
-              <img src="https://tse4.mm.bing.net/th?id=OIP.Z_FV1UU_1YaB7KcXHDa6_gHaKO&pid=Api&P=0&h=180" alt="post" />
-              <img src="https://tse4.mm.bing.net/th?id=OIP.Z_FV1UU_1YaB7KcXHDa6_gHaKO&pid=Api&P=0&h=180" alt="post" />
-
-              </div>
+                <div className="pContent">
+                    <div className="pleft"></div>
+                    <div className="p">POSTS</div>
+                    <div className="pright"></div>
+                </div>
+                <div className="posts">
+                    <img src="https://tse4.mm.bing.net/th?id=OIP.Z_FV1UU_1YaB7KcXHDa6_gHaKO&pid=Api&P=0&h=180" alt="post" />
+                    <img src="https://tse4.mm.bing.net/th?id=OIP.Z_FV1UU_1YaB7KcXHDa6_gHaKO&pid=Api&P=0&h=180" alt="post" />
+                    <img src="https://tse4.mm.bing.net/th?id=OIP.Z_FV1UU_1YaB7KcXHDa6_gHaKO&pid=Api&P=0&h=180" alt="post" />
+                </div>
             </div>
         </div>
     );
